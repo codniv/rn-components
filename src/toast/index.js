5e31fc1e1531fc387ff416d3a1b5cd1c80932c51
dipeshrai123 <raidipesh78@gmail.com>
2021-08-22 19:52:42 +0545

    Toast updated

--- src/toast/index.js
+++ src/toast/index.js
@@ -1,71 +1,96 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import {
   AnimatedBlock,
-  useAnimatedValue,
-  useDrag,
-  clamp,
+  useMountedValue,
+  interpolate,
 } from "react-native-ui-animate";
 
-const { width: wWidth } = Dimensions.get("window");
+import { closeSnack } from "../snack.actions";
+import Icon from "react-native-vector-icons/AntDesign";
+import { colors, fonts, normalize } from "../../modules";
 
-export const Toast = ({ open, close }) => {
-  const my = React.useRef(0);
-  const translateY = useAnimatedValue(open ? 0 : -200);
+const wWidth = Dimensions.get("window").width;
 
-  const bind = useDrag(({ down, movementY }) => {
-    if (down) {
-      my.current = clamp(movementY, -100, 0);
-    } else {
-      if (my.current < -50) {
-        close();
-      } else {
-        my.current = 0;
+export const SnackBar = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const message = "Sample Message for toast";
+  const type = "success"; // or "error"
+
+  const snackMounted = useMountedValue(modalVisible, [0, 1, 0], {
+    onAnimationEnd: (value) => {
+      if (modalVisible && value === 1) {
+        setTimeout(() => {
+          dispatch(closeSnack());
+        }, 3000);
       }
-    }
-
-    translateY.value = my.current;
+    },
+    useNativeDriver: true,
   });
 
-  return (
-    <AnimatedBlock
-      {...bind()}
-      style={[
-        styles.toastContainer,
-        {
-          transform: [{ translateY: translateY.value }],
-        },
-      ]}
-    >
-      <Text
-        style={{
-          textAlign: "center",
-          fontSize: 20,
-        }}
-      >
-        Swipe up to close toast
-      </Text>
-    </AnimatedBlock>
+  return snackMounted(
+    (animation, mounted) =>
+      mounted && (
+        <AnimatedBlock
+          style={[
+            styles.snackContainer,
+            {
+              opacity: animation.value,
+              backgroundColor:
+                type === "success" ? "#4aa255" : colors.light.highlightColor,
+              transform: [
+                {
+                  translateY: interpolate(animation.value, [0, 1], [-120, 0]),
+                },
+              ],
+            },
+          ]}
+        >
+          <View style={styles.snackInfo}>
+            <Text style={styles.snackInfoText}>{message}</Text>
+          </View>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => dispatch(closeSnack())}
+          >
+            <Icon name="closecircle" size={20} color="#ffffff" />
+          </TouchableOpacity>
+        </AnimatedBlock>
+      )
   );
 };
 
 const styles = StyleSheet.create({
-  toastContainer: {
+  snackContainer: {
     position: "absolute",
     width: wWidth - 50,
     left: 50 / 2,
     right: 0,
-    top: 50,
-    height: 100,
-    backgroundColor: "#ffffff",
+    top: 20,
+    backgroundColor: "#4aa255",
     borderRadius: 10,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#e1e1e1",
     shadowOpacity: 0.2,
     shadowColor: "#000",
     shadowRadius: 20,
     shadowOffset: { width: 0, height: 4 },
     padding: 20,
+    flexDirection: "row",
+    elevation: 4,
+  },
+  snackInfo: {
+    flex: 1,
+  },
+  snackInfoText: {
+    fontFamily: fonts.family.AVERTA_SEMI_BOLD,
+    color: "#FFF",
+    fontSize: normalize(fonts.size.thin),
+    flexShrink: 1,
   },
 });
